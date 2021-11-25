873373c7f21ea83064fbda42cdf269051d0b7cbc
Ammar <ammar.kusow2@gmail.com>
2021-11-25 17:02:18 -0500

    add property function

--- LightBnB_WebApp-master/server/database.js
+++ LightBnB_WebApp-master/server/database.js
@@ -26,7 +26,6 @@ const getUserWithEmail = function (email) {
     .query(queryString, [email])
     .then((res) => {
       if (res.rows) {
-        console.log(res.rows);
         return res.rows[0];
       } else {
         return null;
@@ -162,8 +161,6 @@ const getAllProperties = function (options, limit = 10) {
   LIMIT $${queryParams.length};
   `;
 
-  console.log(queryString);
-
   return pool
     .query(queryString, queryParams)
     .then((res) => {
@@ -179,9 +176,33 @@ exports.getAllProperties = getAllProperties;
  * @return {Promise<{}>} A promise to the property.
  */
 const addProperty = function (property) {
-  const propertyId = Object.keys(properties).length + 1;
-  property.id = propertyId;
-  properties[propertyId] = property;
-  return Promise.resolve(property);
+  const queryString = `
+  INSERT INTO properties (
+    title, description, owner_id, cover_photo_url, thumbnail_photo_url, cost_per_night, parking_spaces, number_of_bathrooms, number_of_bedrooms, province, city, country, street, post_code) 
+    VALUES (
+    $1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11,$12,$13,$14)
+    RETURNING *;
+    
+  `;
+  return pool
+    .query(queryString, [
+      property.title,
+      property.description,
+      property.owner_id,
+      property.cover_photo_url,
+      property.thumbnail_photo_url,
+      property.cost_per_night,
+      property.parking_spaces,
+      property.number_of_bathrooms,
+      property.number_of_bedrooms,
+      property.province,
+      property.city,
+      property.country,
+      property.street,
+      property.post_code,
+    ])
+    .then((res) => {
+      return res.rows[0];
+    });
 };
 exports.addProperty = addProperty;
